5f35b9a39004be7f06a5c2ce98f4e824ac3e8ae7
dege23 <douglasanunciacaodasilva@gmail.com>
2023-12-26 00:27:44 +0000

    structure-project

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,12 @@
+const isObj = {
+    ob1: 1,
+    ob2: 2,
+    ob3: 3,
+};
+const helL = hel('Hello').length;
 const hel = (h) => {
-  return h + ", " + "World!";
+    return h + ', ' + 'World!'
 };
 
-console.log(hel("Hello"));
+console.log(hel('Hello'));
+console.log(helL);
